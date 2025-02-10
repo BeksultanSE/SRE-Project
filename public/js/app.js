7eb90032ff5a2bcd45cd61086d32cf48413dbd9d
BEKSULTAN <230599@astanait.edu.kz>
2025-02-10 19:17:50 +0500

    charts in report and some error handling

--- public/js/app.js
+++ public/js/app.js
@@ -28,9 +28,10 @@ const checkAuth = async () => {
         });
         console.log(response);
         if (response.ok) {
-                        
+            
         } else {
-            alert("You are not authorized for this app.");
+            showToast("You are not authorized for this app.", "danger");
+            //alert("You are not authorized for this app.");
             window.location.href = '/login';
         }
     } catch (error) {
